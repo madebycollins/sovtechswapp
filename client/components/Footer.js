eef9e38f8efb7f5a43481dcebe1a0b8f0745e943
Collins Zimba <dev@madebycollins.com>
2021-12-20 01:13:57 +0200

    Updated Footer component

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer>
-            <a>
+            <a href="https://www.github.com/madebycollins" target="_blank">
                 Made by Collins Zimba
             </a>
         </footer>
