6fe9b2e1544a57e3015f631acf2256404d4622ab
Collins Zimba <dev@madebycollins.com>
2021-12-19 17:42:40 +0200

    Added function to get people

--- server/src/datasources/starwars.js
+++ server/src/datasources/starwars.js
@@ -6,6 +6,22 @@ class StarwarsAPI extends RESTDataSource {
         // The URL for the REST API to envelope
         this.baseURL = 'https://swapi.dev/api/';
     }
+
+    async getAllPeople() {
+        // GET people from the API
+        const response = await this.get('people');
+
+        // Empty array to store the data
+        let data = []
+
+        // Populate the array if we got an array back
+        if (Array.isArray(response)) {
+            data = response.map(person => this.personReducer(person))
+        }
+
+        // Return the data
+        return data
+    }
 }
 
 module.exports = StarwarsAPI;
