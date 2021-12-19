1f0e0ac5b120aa413dcac9b26e7cb13604296df5
Collins Zimba <dev@madebycollins.com>
2021-12-19 17:27:00 +0200

    Added field descriptions to the schema definition

--- server/src/schema.js
+++ server/src/schema.js
@@ -2,10 +2,15 @@ const {gql} = require('apollo-server');
 
 const typeDefs = gql`
     type Person {
+        "The name of the person"
         name: String
+        "The height of the person"
         height: String
+        "The mass of the person"
         mass: String
+        "The gender of the person"
         gender: String
+        "The homeworld of the person"
         homeworld: String
     }
 
