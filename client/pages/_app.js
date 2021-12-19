f261584fdd5e73985f27a9f437c7512eec0a32fd
Collins Zimba <dev@madebycollins.com>
2021-12-20 00:53:09 +0200

    Added Bulma CSS import to layout file

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,6 @@
 import Layout from '../components/Layout'
 import "../styles/styles.css"
+import 'bulma/css/bulma.min.css';
 
 export default function MyApp({ Component, pageProps }) {
     return (
