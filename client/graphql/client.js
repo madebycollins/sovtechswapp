248af1b507e68efb6485093be9a7950d23495f66
Collins Zimba <dev@madebycollins.com>
2021-12-20 12:38:11 +0200

    Added Apollo Client logic for error handling

--- client/graphql/client.js
+++ client/graphql/client.js
@@ -1,11 +1,29 @@
 import {
-    ApolloClient,
+    ApolloClient, ApolloLink, HttpLink,
     InMemoryCache,
 } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors)
+        graphQLErrors.forEach(({ message, locations, path }) =>
+            console.log(
+                `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+            )
+        );
+    if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+const httpLink = new HttpLink({ uri: 'https://xuk2u.sse.codesandbox.io/' })
+
+const link = ApolloLink.from([
+    errorLink,
+    httpLink,
+]);
 
 const client = new ApolloClient({
-    uri: 'https://xuk2u.sse.codesandbox.io/',
-    cache: new InMemoryCache()
+    cache: new InMemoryCache(),
+    link: link
 });
 
 module.exports = client
