6e52f6e52082b58f06cfa1d707d660e1f17d1344
Collins Zimba <dev@madebycollins.com>
2021-12-21 00:41:50 +0200

    Added an About page

---
+++ client/pages/about.js
@@ -0,0 +1,18 @@
+export default function About() {
+    return (
+        <div>
+            <h1 className={"title"}>Work by Collins Zimba</h1>
+            <p>
+                This application is a Nextjs application built as part of a technical assessment for Sovtech
+            </p>
+            <br/>
+            <p>
+                The frontend application communicates with a GraphQL backend to retrieve Star Wars character data
+            </p>
+            <br/>
+            <p>
+                Enjoy!
+            </p>
+        </div>
+    )
+}
