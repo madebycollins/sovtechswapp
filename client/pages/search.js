d426354bbcceed3b45eba0c83a8ebd752808f2b7
Collins Zimba <dev@madebycollins.com>
2021-12-20 10:24:09 +0200

    Fixed button CSS not applying

--- client/pages/search.js
+++ client/pages/search.js
@@ -3,13 +3,16 @@ export default function Home() {
         display: "flex",
         flexDirection: "row",
         justifyContent: "between",
-        marginLeft: "10px"
+    }
+
+    const button_style = {
+        marginLeft: "20px"
     }
 
     return (
         <div style={style}>
             <input className="input" type="text" placeholder="Search..." />
-            <div className="field is-grouped">
+            <div style={button_style} className="field is-grouped">
                 <div className="control">
                     <button className="button is-link">Submit</button>
                 </div>
