a08a820699a61079ebaa3fa4f724fe00678e9f71
Collins Zimba <dev@madebycollins.com>
2021-12-20 04:12:13 +0200

    Added visuals for naviagating between pages

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,18 +1,33 @@
 import { useQuery, gql } from "@apollo/client";
 import { GET_PEOPLE } from "../graphql/queries";
 import CharacterCard from "../components/CharacterCard";
+import { useRouter } from 'next/router'
 
 export default function Home() {
-    const { loading, error, data } = useQuery(GET_PEOPLE);
+    // Get the page value
+    const router = useRouter()
+    let { page } = router.query
+
+    // Prepare the page number
+    page = Number.parseInt(page) || 1
+
+    // Load data for the relevant page
+    const { loading, error, data } = useQuery(GET_PEOPLE, {
+        variables: { page },
+    });
 
     if (loading) return ('Loading...');
     if (error) return (`Error! ${error.message}`);
 
   return (
     <div className="">
-        {data.people.map( person => (
-            <CharacterCard person={person} />
+        {data.people.map( (person, index) => (
+            <CharacterCard key={index} person={person} />
         ))}
+        <nav className="pagination" role="navigation" aria-label="pagination">
+            <a className="pagination-previous">Previous</a>
+            <a className="pagination-next">Next page</a>
+        </nav>
     </div>
   )
 }
