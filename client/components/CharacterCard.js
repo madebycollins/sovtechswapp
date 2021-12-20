0220ba834db4527917212d0adc6c865fe49622d3
Collins Zimba <dev@madebycollins.com>
2021-12-20 03:16:17 +0200

    Added Character Card component

---
+++ client/components/CharacterCard.js
@@ -0,0 +1,27 @@
+export default function CharacterCard(props) {
+    return (
+        <div className="card is-8">
+            <div className="card-content">
+                <div className="media">
+                    <div className="media-content">
+                        <p className="title is-4">{props.person.name}</p>
+                    </div>
+                </div>
+
+                <div className="content">
+                    <a>Height</a>: {props.person.height}
+                    <br />
+                    <a>Mass</a>: {props.person.mass}
+                    <br />
+                    <a>Gender</a>: {props.person.gender}
+                    <br />
+                    <a>Home World</a>: {props.person.homeworld}
+                    <br />
+                </div>
+                <div className="">
+                    Footer
+                </div>
+            </div>
+        </div>
+    )
+}
