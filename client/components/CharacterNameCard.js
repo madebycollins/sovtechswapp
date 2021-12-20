82f5755180bad4f7f8b2c7be707a251006b8dff7
Collins Zimba <dev@madebycollins.com>
2021-12-20 13:49:57 +0200

    Added CharacterNardCard component

---
+++ client/components/CharacterNameCard.js
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import { Button } from "react-bulma-components";
+export default function CharacterNameCard(props) {
+    return (
+            <div className="card is-8">
+                <div className="card-content">
+                    <div className="media">
+                        <div className="media-content">
+                            <p className="title is-4">{props.person.name}</p>
+                        </div>
+                    </div>
+                    <div className="content">
+                        <Link href={"/detail/" + props.person.name}>
+                            <Button color="primary">View Detail</Button>
+                        </Link>
+                    </div>
+                </div>
+
+            </div>
+    )
+}
