17819b83f6021f51dc8d17bd3a48f4c94ad09b48
Collins Zimba <dev@madebycollins.com>
2021-12-20 02:15:24 +0200

    Injected Apollo client into all pages

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,11 +1,16 @@
 import Layout from '../components/Layout'
 import "../styles/styles.css"
 import 'bulma/css/bulma.min.css';
+import client from "../graphql/client"
+
+import { ApolloProvider } from "@apollo/client";
 
 export default function MyApp({ Component, pageProps }) {
     return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+        <ApolloProvider client={client}>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </ApolloProvider>
     )
 }
