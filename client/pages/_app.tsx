461a38a95ca7ef0bba9982d91515675fa45d0914
Collins Zimba <dev@madebycollins.com>
2021-12-21 20:08:35 +0200

    Converted _app to tsx

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -3,10 +3,11 @@ import "../styles/styles.css"
 import "../styles/loader.css"
 import 'bulma/css/bulma.min.css';
 import client from "../graphql/client"
+import type { AppProps } from 'next/app'
 
 import { ApolloProvider } from "@apollo/client";
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }: AppProps) {
     return (
         <ApolloProvider client={client}>
             <Layout>
